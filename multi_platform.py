22ef1ec73183ae203bf3b10599564ef0b589e968
hardik-vaghela05 <hardikv682@gmail.com>
2025-12-26 16:12:40 +0530

    indeed

--- multi_platform.py
+++ multi_platform.py
@@ -29,7 +29,7 @@ class JobScraper:
         options.add_experimental_option("prefs", prefs)
         
         # Standard Headless Options
-        # options.add_argument('--headless=new')
+        options.add_argument('--headless=new')
         options.add_argument('--window-size=1920,1080')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
